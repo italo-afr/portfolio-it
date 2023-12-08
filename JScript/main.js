883b8e1ad9fd13626135ef8dc34eb4ba0a2711f4
italo-afr <italoafr1@gmail.com>
2023-12-07 23:23:42 -0300

    Adicionando alguns projetos em destaque.

--- JScript/main.js
+++ JScript/main.js
@@ -1,3 +1,5 @@
+
+
 let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
 
@@ -10,10 +12,10 @@ const sr = ScrollReveal ({
     distance: '65px',
     duration: 2600,
     delay: 450,
-    reset: true
+    reset: false
 });
 
 sr.reveal('.hero-text',{delay:200, origin:'top'});
 sr.reveal('.hero-img',{delay:450, origin:'top'});
 sr.reveal('.icons',{delay:500, origin:'left'});
-sr.reveal('.scroll-down',{delay:500, origin:'right'});
+sr.reveal('.scroll-down',{delay:500, origin:'bottom'});
