168cbf8e51e48cfb854d71925dbaf6b27e830fd6
italo-afr <italoafr1@gmail.com>
2023-12-08 11:44:36 -0300

    Adicionando o rodapé, mais algumas alterações no cards

--- JScript/main.js
+++ JScript/main.js
@@ -1,21 +1,48 @@
-
-
 let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
 
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navlist.classList.toggle('open');
-}
+};
 
-const sr = ScrollReveal ({
+const sr = ScrollReveal({
     distance: '65px',
     duration: 2600,
     delay: 450,
     reset: false
 });
 
-sr.reveal('.hero-text',{delay:200, origin:'top'});
-sr.reveal('.hero-img',{delay:450, origin:'top'});
-sr.reveal('.icons',{delay:500, origin:'left'});
-sr.reveal('.scroll-down',{delay:500, origin:'bottom'});
+sr.reveal('.hero-text', { delay: 200, origin: 'top' });
+sr.reveal('.hero-img', { delay: 450, origin: 'top' });
+sr.reveal('.icons', { delay: 500, origin: 'left' });
+sr.reveal('.scroll-down', { delay: 500, origin: 'bottom' });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const scrollDownElement = document.querySelector(".scroll-down");
+    const footerElement = document.querySelector(".footer");
+
+    window.addEventListener("scroll", () => {
+        const footerPosition = footerElement.getBoundingClientRect().top;
+
+        scrollDownElement.classList.toggle("hidden", footerPosition <= window.innerHeight);
+    });
+});
+
+const nav = document.querySelector('.nav');
+let lastScrollTop = 0;
+const scrollThreshold = 50;
+
+window.addEventListener('scroll', () => {
+  const currentScrollTop = window.scrollY;
+
+  if (currentScrollTop > lastScrollTop && currentScrollTop > scrollThreshold) {
+    // Scrolling down, hide navigation
+    nav.classList.add('nav-hidden');
+  } else {
+    // Scrolling up or at the top, show navigation
+    nav.classList.remove('nav-hidden');
+  }
+
+  lastScrollTop = currentScrollTop;
+});
